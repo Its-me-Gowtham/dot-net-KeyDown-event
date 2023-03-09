fb43064a10e696595b3e7b5d208f38a6a33019a2
Its-me-Gowtham <gowthamkv001@gmail.com>
2023-03-09 18:37:14 +0530

    firstcommit

---
+++ KeyDown event/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Update the Label with the last key pressed
+            LastKeyPressedLabel.Content = $"Last Key Pressed: {e.Key.ToString()}";
+        }
+    }
+}
