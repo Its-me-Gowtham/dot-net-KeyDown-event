e490a47bfae6bd5b12f8af71651d11d0ca37faf6
Its-me-Gowtham <gowthamkv001@gmail.com>
2023-03-10 21:12:52 +0530

    addedtimeout

--- KeyDown event/MainWindow.xaml.cs
+++ KeyDown event/MainWindow.xaml.cs
@@ -1,20 +1,35 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(3000);
+            timer.Tick += Timer_Tick;
         }
 
         private void InputTextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            // Update the Label with the last key pressed
             LastKeyPressedLabel.Content = $"Last Key Pressed: {e.Key.ToString()}";
+            LastKeyPressedLabel.Visibility = Visibility.Visible;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs f)
+        {
+            timer.Stop();
+            LastKeyPressedLabel.Visibility = Visibility.Hidden;
+            LastKeyPressedLabel.Content = string.Empty;
         }
     }
 }
